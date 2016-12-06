107de56826330617fabb01f87ba13bc914979b04
Teddy VALETTE <teddy.valette1@etu.univ-lorraine.fr>
2016-12-06 09:24:04 +0100

    Modification des modeles

--- src/giftbox/models/Categorie.php
+++ src/giftbox/models/Categorie.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: valette15u
- * Date: 05/12/2016
- * Time: 10:57
- */
 
 namespace giftbox\models;
-
 
 class Categorie extends \Illuminate\Database\Eloquent\Model
 {
--- src/giftbox/models/Prestation.php
+++ src/giftbox/models/Prestation.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace giftbox\models;
-
 
 class Prestation extends \Illuminate\Database\Eloquent\Model
 {
