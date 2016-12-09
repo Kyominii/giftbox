c40c33ee6fee07c923b678442b1ae211d9620b4f
Teddy VALETTE <teddy.valette1@etu.univ-lorraine.fr>
2016-12-09 09:50:10 +0100

    Ajout des fichiers du framework CSS pour BBBank

---
+++ src/BBBank/view/home.php
@@ -0,0 +1,13 @@
+<?php
+if(!isset($_SESSION))
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BBBank | <?php echo $title_suffix ?></title>
+        <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+        <script src="semantic/dist/semantic.min.js"></script>
+    </head>
+</html>
